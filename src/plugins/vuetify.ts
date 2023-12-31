a13ecf36ad935220b03b3e01b51cd93fdc5bc680
Mario Simão <mariosimao@poli.ufrj.br>
2023-12-31 15:20:58 -0300

    Static sige generation (SSG)

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -15,6 +15,7 @@ import { pt } from 'vuetify/locale'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  ssr: true,
   theme: {
     themes: {
       light: {
