0c714ab0a24929e21e5ba1fb85b473e01bf426a7
Mario Simão <mariosimao@poli.ufrj.br>
2023-12-31 17:35:21 -0300

    Adapt CI to Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
   //...
   build: {
     transpile: ['vuetify'],
