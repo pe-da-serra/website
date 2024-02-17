de980fefb65eb94afc57bd8d3d0338443005df2b
Mario Simão <mariosimao@poli.ufrj.br>
2024-02-17 18:13:58 -0300

    Add double room with shared bathroom photos

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -40,11 +40,12 @@ import RoomCard from '@/components/RoomCard.vue';
 import { routeNames } from '@/router';
 import img1 from '@/assets/example/img-1.jpg';
 import doubleImg from '@/assets/double-1.jpg';
+import doubleShareBathroomImg from '@/assets/double-shared-bathroom-1.jpg';
 
 const rooms = ref([
   { title: "Solteiro", guests: 1, beds: 1, route: routeNames.room.single, image: img1 },
   { title: "Duplo", guests: 2, beds: 2, route: routeNames.room.double, image: doubleImg },
-  { title: "Duplo banheiro compartilhado", guests: 2, beds: 2, route: routeNames.room.doubleSharedBathroom, image: img1 },
+  { title: "Duplo banheiro compartilhado", guests: 2, beds: 2, route: routeNames.room.doubleSharedBathroom, image: doubleShareBathroomImg },
   { title: "Casal", guests: 2, beds: 1, route: routeNames.room.couple, image: img1 },
   { title: "Triplo", guests: 3, beds: 3, route: routeNames.room.triple, image: img1 },
   { title: "Apartamento", guests: 4, beds: 3, route: routeNames.room.apartment, image: img1 },
