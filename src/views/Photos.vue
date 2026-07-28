1453636a9981a3c152e9f80761f51afb2c10ac20
Mario Simão <mariogsimao@gmail.com>
2026-07-28 19:01:13 -0300

    feat: update region photos on galery

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -65,11 +65,16 @@ import room2 from '@/assets/double-1.jpg';
 import room3 from '@/assets/double-shared-bathroom-1.jpg';
 import room4 from '@/assets/couple-1.jpg';
 import room5 from '@/assets/apartment-1.jpg';
-import region1 from '@/assets/region-1.jpg';
-import region2 from '@/assets/region-2-lg.jpg';
+// import region1 from '@/assets/region-1.jpg';
+// import region2 from '@/assets/region-2-lg.jpg';
 import region3 from '@/assets/region-3-lg.jpg';
 import region4 from '@/assets/region-4-lg.jpg';
-import region5 from '@/assets/region-5-lg.jpg';
+// import region5 from '@/assets/region-5-lg.jpg';
+import region6 from '@/assets/region-6-lg.jpg';
+import region7 from '@/assets/region-7-lg.jpg';
+import region8 from '@/assets/region-8-lg.jpg';
+
+
 import { computed } from 'vue';
 
 // Tabs
@@ -90,11 +95,14 @@ const images = ref([
   { source: room3, tags: [ Tab.Rooms ] },
   { source: room4, tags: [ Tab.Rooms ] },
   { source: room5, tags: [ Tab.Rooms ] },
-  { source: region1, tags: [ Tab.Region ] },
-  { source: region2, tags: [ Tab.Region ] },
+  { source: region6, tags: [ Tab.Region ] },
+  { source: region7, tags: [ Tab.Region ] },
+  { source: region8, tags: [ Tab.Region ] },
+  // { source: region1, tags: [ Tab.Region ] },
+  // { source: region2, tags: [ Tab.Region ] },
   { source: region3, tags: [ Tab.Region ] },
   { source: region4, tags: [ Tab.Region ] },
-  { source: region5, tags: [ Tab.Region ] },
+  // { source: region5, tags: [ Tab.Region ] },
 ]);
 const filteredImages = computed(() => {
   let imagesWithIndex = images.value.map((img, idx) => ({ ...img, index: idx}));
